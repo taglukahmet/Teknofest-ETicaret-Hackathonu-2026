d7147214327e36973e486a76ea774425df28662a
Boras1995 <borasapci@gmail.com>
2026-07-10 10:37:03 +0200

    data_pipeline datasini sildim, data folderindaki gerekli data preperation datalarini düzelttim.

--- src/data/format_migration.py
+++ src/data/format_migration.py
@@ -1,6 +1,55 @@
-def convert_csv_to_parquet(data_dir: str) -> None:
-    """
-    (Task: Member 1)
-    Reads raw CSVs from data_dir and writes them to disk as columnar Parquet files.
-    """
-    pass
+import polars as pl
+
+###
+#Görev 1) Veri Formatlama
+###
+
+#tahmin ettirmeden typelari biz veriyoruz. farkliligin az oldugu yerlerde Utf8(string) yerine categorical kullaniyoruz ki  tasarruf edelim
+items_schema = {
+    "item_id" : pl.Utf8,
+    "title" : pl.Utf8,
+    "category" : pl.Utf8,
+    "brand" : pl.Categorical,
+    "gender" : pl.Categorical,
+    "age_group" : pl.Categorical,
+    "attributes" : pl.Utf8
+}
+
+terms_schema = {
+    "term_id" : pl.Utf8,
+    "query" : pl.Utf8
+}
+
+training_schema = {
+    "id" : pl.Utf8,
+    "term_id" : pl.Utf8,
+    "item_id" : pl.Utf8,
+    "label" : pl.Int64 #neden int 64? sadece 1se neden int8 ya da daha iyisi bool degil?
+}
+
+submission_schema = {
+    "id": pl.Utf8,
+    "term_id": pl.Utf8,
+    "item_id": pl.Utf8,
+}
+
+sample_sub_schema = {
+    "id": pl.Utf8,
+    "prediction": pl.Int64,
+}
+
+
+
+files = {
+    "items": ("items.csv", items_schema),
+    "terms": ("terms.csv", terms_schema),
+    "training_pairs": ("training_pairs.csv", training_schema),
+    "submission_pairs": ("submission_pairs.csv", submission_schema),
+    "sample_submission": ("sample_submission.csv", sample_sub_schema),
+}
+
+#hepsini Parquete ceviriyoruz:
+for name, (path, schema) in files.items():
+    df = pl.read_csv(path, schema=schema)
+    df.write_parquet(f"{name}.parquet")
+    #print(name, df.shape, df.estimated_size("mb"), "MB") #beklenen boyut
